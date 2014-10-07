89ab808d3d405e7a897192b5c0af687a3f443cee
Jim Ray <jimiray@gmail.com>
2014-10-06 20:51:40 -0600

    update host url

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   # Mail host
-  config.action_mailer.default_url_options = { host: "example.com" }
+  config.action_mailer.default_url_options = { host: "nurdgurls.com" }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address:              "smtp.mandrillapp.com",
