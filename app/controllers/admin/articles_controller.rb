46f2ccbba53f3ea331bfc9afbff3552b504012e0
Jim Ray <jimiray@gmail.com>
2014-10-17 11:49:29 -0600

    fix for missing published_at

--- app/controllers/admin/articles_controller.rb
+++ app/controllers/admin/articles_controller.rb
@@ -63,7 +63,7 @@ module Admin
     end
 
     def article_params
-      params.require(:article).permit(:title, :preview, :body, :featured, :category_id, :image, published_at)
+      params.require(:article).permit(:title, :preview, :body, :featured, :category_id, :image, :published_at)
     end
 
   end
